be6bbc5dd0fc20e82ec2a431523060430c278880
Kobita <161524322+kobita1122@users.noreply.github.com>
2026-01-17 22:38:35 +0600

    Create connect.js

---
+++ connect.js
@@ -0,0 +1,9 @@
+async function connectWallet() {
+  if (!window.ethereum) {
+    alert("MetaMask not found");
+    return;
+  }
+
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+  setupProvider();
+}
